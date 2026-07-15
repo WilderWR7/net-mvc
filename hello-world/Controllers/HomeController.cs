ee8847d1970ebf925de14e6ad5983e3497178431
WilderWR7 <wilder.mayta91@gmail.com>
2026-07-15 08:13:47 -0400

    first commit

---
+++ hello-world/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using hello_world.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace hello_world.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ hello-world/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace hello_world.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
